- Optimize logging - Fix GICS code extraction - Add multiple mixed model weighting schemes - Move matplotlib and pandas configs to configs.py - Add table .css file

core/analysis.py
@@ -2,19 +2,14 @@
 This module implements classes and methods for data analysis
 """
 import json
-import os
 import webbrowser
 
 import numpy as np
 import pandas as pd
 from matplotlib import ticker
-from pandas import DataFrame
-import matplotlib.pyplot as plt
-from scipy.signal import savgol_filter
 
-from config import ROOT_DIR
+from config import *
 from core.data_collection import add_constituency_col, to_actual_index, load_full_data
-from core.utils import get_study_period_ranges
 
 
 class StatsReport:
@@ -36,77 +31,122 @@ class StatsReport:
         else:
             self.attrs = attrs
 
-    def to_html(self):
+    def to_html(self, title='Statistics', file_name='stats', open_window=True):
+        """
+        Create and save HTML table
+
+        :param title: Title of HTML page
+        :param file_name: File name to save HTML page to
+        :param open_window: Open created file in new browser window
+        :return:
+        """
         data = self.split_dict_cols()
-        style = """
+        table = data.to_html(classes='blueTable')
+        html_doc = f"""
         <html>
-            <head><title>Statistics</title></head>
+            <head><title>{title}</title></head>
             <link rel="stylesheet" type="text/css" media="screen" href="tbl_style.css" />
             <body>
                 {table}
             </body>
         </html>
         """
-        with open(os.path.join(ROOT_DIR, 'data', 'stats.html'), 'w') as f:
-            f.write(style.format(table=data.to_html(classes='blueTable')))
-        webbrowser.open(os.path.join(ROOT_DIR, 'data', 'stats.html'), new=2)
+        with open(os.path.join(ROOT_DIR, 'data', f'{file_name}.html'), 'w') as f:
+            f.write(html_doc)
 
-    def summary(self, score_list: list = None, k: int = None, last_only=True, by_model_type=False):
+        if open_window:
+            webbrowser.open(os.path.join(ROOT_DIR, 'data', f'{file_name}.html'), new=2)
+
+    def summary(self, score_list: list = None, index_only: str = None, k: int = None, last_only=True,
+                by_model_type=False, sort_by: list = None,
+                show_std=False, to_html=True, open_window=True) -> pd.DataFrame:
         """
-        Print summary of StatsReport
+        Print summary of StatsReport and return as DataFrame
 
-        :param model_type:
-        :param by_model_type:
-        :param k:
-        :param score_list:
-        :param last_only:
-        :return:
+        :param to_html: Export summary table to HTML file
+        :param open_window: Open created HTML page in new browser window
+        :param index_only: Filter by specified index
+        :param show_std: Show standard deviation along with mean
+        :param sort_by: List of column indices to sort by
+        :param by_model_type: Show results separately for each model
+        :param k: Top-k statistics to consider
+        :param score_list: List of performance metrics to include
+        :param last_only: Show statistics for last run only
+        :return: Summary as DataFrame
         """
 
         data = self.split_dict_cols(drop_configs=True)  # Split dict columns
+        index_name = None
 
         # JOB: Select relevant columns
         if score_list:
-            columns = [col for col in data.columns if any([col.startswith(score) for score in score_list])]
+            columns = [col for col in data.columns if any([score in col for score in score_list])]
             if k:
-                columns = [col for col in columns if col.endswith(str(k))]
+                columns = [col for col in columns if col.endswith(f'_{k}')]
         else:
             columns = data.columns
 
+        if index_only:
+            # Filter by specified index
+            index_name = data.get('Index Name').loc[
+                data['Index Name'].str.contains(index_only, case=False, regex=False)].drop_duplicates().values[0]
+
+            print(f'Filter by index: {index_name}')
+            data = data.loc[data['Index Name'].str.contains(index_name, case=False, regex=False)]
+
         # JOB: Print summary info
-        if not last_only:
+        if last_only:
+            # Filter by last available index
+            print(f'Summary report for last available run (ID={last_id(data)} of {self.last_id}) and \n'
+                  f'{columns}\n')
+            try:
+                data = data.loc[data['ID'] == last_id(data)]
+            except TypeError as te:
+                print(te)
+                pass
+        else:
             print(f'Summary report for all runs and \n'
                   f'{columns}\n')
+
+        n_models = data['Model Type'].nunique()
+        if data.shape[0] > n_models:
+            print(f'Showing last {n_models} out of {data.shape[0]}')
+            print('...')
+            print(data.tail(n_models))
         else:
-            print(f'Summary report for last run (ID={self.last_id}) and \n'
-                  f'{columns}\n')
+            print(data)
+
+        total_obs = len(data['Test Set Start Date'].unique())
+        print(f'Number of unique time periods in filtered observation data: {total_obs}')
 
         # JOB: Print summary statistics for relevant columns
         if not by_model_type:
             for attr in columns:
-                try:
-                    if last_only:
-                        print(Statistics(data.loc[data['ID'] == self.last_id, attr], name=attr))
-                    else:
-                        print(Statistics(data.get(attr), name=attr))
-                except TypeError as te:
-                    pass
-
+                print(Statistics(data.get(attr), name=attr))
         else:
-
             df = pd.DataFrame({attr: [] for attr in columns})
             df.index.name = 'Model'
             for attr in columns:
-                if last_only:
-                    data = data.loc[data['ID'] == self.last_id]
-
                 model_types = data['Model Type'].unique()
                 for model_type in model_types:
                     model_data = data.loc[data['Model Type'] == model_type]
                     df.loc[model_type, attr] = model_data[attr].mean()
-                    df.loc[model_type, f'{attr}_std'] = model_data[attr].std()
+                    if show_std:
+                        df.loc[model_type, f'{attr}_std'] = model_data[attr].std()
 
+            # JOB: Sort columns lexicographically:
+            df = df.reindex(sorted(df.columns), axis=1)
+            if sort_by and k:
+                df.sort_values(by=[f'{key}_{k}' for key in sort_by], axis=0, ascending=False, inplace=True)
+            else:
+                df.sort_values(by=columns, axis=0, ascending=False, inplace=True)
             print(df)
+            if to_html:
+                df_to_html(df,
+                           title='Model Performance Overview' if not index_only else f'Model Performance Overview {index_name}',
+                           open_window=open_window)
+
+            return df
 
     def split_dict_cols(self, drop_configs=True):
         """
@@ -130,7 +170,7 @@ class StatsReport:
 
     @property
     def last_id(self):
-        return self.data.get('ID').max()
+        return int(self.data.get('ID').max())
 
 
 class Statistics:
@@ -139,6 +179,13 @@ class Statistics:
     """
 
     def __init__(self, data=None, index=None, name=None):
+        """
+        Constructor for Statistics class
+
+        :param data: Series-like data object
+        :param index: (Optional) Index for data series
+        :param name: Name of data series
+        """
         self.name = name
         if hasattr(data, 'index'):
             index = data.index
@@ -148,13 +195,17 @@ class Statistics:
         summary = None
         if self.data is not None:
             summary = f'{30 * "-"}\n' \
-                f'Name: {self.name}\n' \
-                f'Count: {self.data.count()}\n' \
-                f'Unique: {self.data.nunique()}\n' \
-                f'Mean: {np.round(self.mean, 4)}\n' \
-                f'Stdv.: {np.round(self.std, 4)}\n' \
-                f'{30 * "-"}\n'
+                      f'Name: {self.name}\n' \
+                      f'Count: {self.data.count}\n' \
+                      f'Unique: {self.nunique}\n' \
+                      f'Mean: {np.round(self.mean, 4)}\n' \
+                      f'Stdv.: {np.round(self.std, 4)}\n' \
+                      f'{30 * "-"}\n'
         return summary
+
+    @property
+    def count(self):
+        return pd.Series.count(self.data)
 
     @property
     def mean(self):
@@ -203,7 +254,7 @@ class VisTable:
 
         if self.stat in ['count', 'share', 'percentage']:
             ax.yaxis.set_major_formatter(ticker.PercentFormatter(1.0, decimals=0))
-            ax.set_ylim(0, 0.3)
+            ax.set_ylim(0, 0.35)
         if 'return' in self.attrs:
             ax.yaxis.set_major_formatter(ticker.PercentFormatter(1.0, decimals=1))
 
@@ -221,7 +272,7 @@ class VisTable:
                         label=key)
             else:
                 pass
-
+        ax.set_title(', '.join([attr.replace('_', ' ').title() for attr in self.attrs]), fontsize=16)
         legend = ax.legend(loc='best', fontsize='large')
         for line in legend.get_lines():
             line.set_linewidth(3.0)
@@ -253,6 +304,11 @@ class DataTable:
         return self.data.loc[self.time_frame[0]:self.time_frame[1]]
 
     def get_group_stats(self):
+        """
+        Create DataFrame containing grouped stats
+
+        :return:
+        """
         summary = pd.DataFrame([], columns=['mean', 'std', 'skew', 'kurt', 'count'])
         summary.index.name = 'Industry'
 
@@ -324,10 +380,25 @@ def resample_month_end(data, columns: list = None):
     return data
 
 
-def df_to_html(df, file_name='table', open_window=True):
-    style = """
+def df_to_html(df, title='Statistics', file_name='table', open_window=True) -> None:
+    """
+    Export DataFrame to HTML page
+
+    :param df: DataFrame to export as HTML
+    :param title: HTML page title
+    :param file_name: File name to save HTML to
+    :param open_window: Open HTML page in new browser window
+    :return: None
+    """
+    try:
+        table = df.to_html(classes='blueTable')
+    except AttributeError as ae:
+        print('Single statistic cannot be exported to HTML.')
+        return
+
+    html_doc = f"""
     <html>
-        <head><title>Statistics</title></head>
+        <head><title>{title}</title></head>
         <link rel="stylesheet" type="text/css" media="screen" href="tbl_style.css" />
         <body>
             {table}
@@ -335,13 +406,31 @@ def df_to_html(df, file_name='table', open_window=True):
     </html>
     """
     with open(os.path.join(ROOT_DIR, 'data', f'{file_name}.html'), 'w') as f:
-        f.write(style.format(table=df.to_html(classes='blueTable')))
+        f.write(html_doc)
 
     if open_window:
         webbrowser.open(os.path.join(ROOT_DIR, 'data', f'{file_name}.html'), new=2)
 
 
-if __name__ == '__main__':
+def last_id(data):
+    try:
+        last = int(data.get('ID').max())
+    except ValueError:
+        last = None
+        print('Index not in DataFrame.')
+    return last
+
+
+def main(full_log=False, index_summary=True, plots_only=False):
+    """
+    Main method of analysis.py module (to be called when module is run stand-alone)
+
+    :param full_log:
+    :param index_summary: Whether to include summary of index
+    :param plots_only: Whether to only plot index constituency and daily returns over time
+                       (only considered if index_summary is True)
+    :return:
+    """
     configs = json.load(open(os.path.join(ROOT_DIR, 'config.json'), 'r'))
     cols = ['above_cs_med', *configs['data']['columns']]
 
@@ -354,34 +443,50 @@ if __name__ == '__main__':
     }
 
     # JOB: Specify index ID, relevant columns and study period length
-    index_id = index_dict['DJ600']
+    index_id = index_dict['DAX']
 
-    # Load full index data
-    constituency_matrix, full_data, index_name, folder_path = load_full_data(index_id=index_id,
-                                                                             force_download=False,
-                                                                             last_n=None, columns=cols.copy(),
-                                                                             merge_gics=True)
+    # JOB: Create model performance report
+    report = StatsReport()
+    if full_log:
+        report.to_html(file_name='run_overview', title='Training Log', open_window=False)
+    report.summary(last_only=True, index_only=None, score_list=['Accuracy', 'Sharpe', 'Sortino', 'Excess Return'],
+                   k=10,
+                   by_model_type=True, sort_by=['Top-k Annualized Sharpe'], show_std=False, to_html=True,
+                   open_window=False)
 
-    # JOB: Add constitency column and reduce to actual index constituents
-    full_data = add_constituency_col(full_data, folder_path)
-    full_data = to_actual_index(full_data)
-    full_data.set_index('datadate', inplace=True)
+    if index_summary:
+        # Load full index data
+        constituency_matrix, full_data, index_name, folder_path = load_full_data(index_id=index_id,
+                                                                                 force_download=False,
+                                                                                 last_n=None, columns=cols.copy(),
+                                                                                 merge_gics=True)
 
-    vis = VisTable(full_data, time_frame=('2002-01', '2019-12'), groups=['gics_sector'], freq='D', stat='count',
-                   attrs=['gvkey'])
-    vis.plot_grouped()
+        # JOB: Add constitency column and reduce to actual index constituents
+        full_data = add_constituency_col(full_data, folder_path)
+        full_data = to_actual_index(full_data)
+        full_data.set_index('datadate', inplace=True)
 
-    vis = VisTable(full_data, time_frame=('2002-01', '2019-12'), groups=['gics_sector'], freq='Y',
-                   stat='mean',
-                   attrs=['daily_return'])
-    vis.plot_grouped()
+        vis = VisTable(full_data, time_frame=('2002-01', '2019-12'), groups=['gics_sector'], freq='D', stat='count',
+                       attrs=['gvkey'])
+        vis.plot_grouped()
 
-    # JOB: Resample to monthly
-    data = resample_month_end(full_data)
-    dt = DataTable(data=data, attrs=['monthly_return'], time_frame=('2002-01', '2019-12'), groups=['gics_sector'],
-                   freq='M')
+        vis = VisTable(full_data, time_frame=('2002-01', '2019-12'), groups=['gics_sector'], freq='Y',
+                       stat='mean',
+                       attrs=['daily_return'])
+        vis.plot_grouped()
 
-    stats = dt.get_group_stats()
-    print(stats)
+        if not plots_only:
+            # JOB: Resample to monthly
+            data = resample_month_end(full_data)
+            dt = DataTable(data=data, attrs=['monthly_return'], time_frame=('2002-01', '2019-12'),
+                           groups=['gics_sector'],
+                           freq='M')
 
-    df_to_html(stats)
+            stats = dt.get_group_stats()
+            print(stats)
+
+            df_to_html(stats, title='Index Stats', file_name='index_stats', open_window=False)
+
+
+if __name__ == '__main__':
+    main(full_log=True, index_summary=False, plots_only=True)

core/execute.py
@@ -1,5 +1,3 @@
-import random
-
 __author__ = "Sven Köpke"
 __copyright__ = "Sven Köpke 2019"
 __version__ = "0.0.1"
@@ -7,14 +5,13 @@ __license__ = "MIT"
 
 import datetime
 import json
+import random
 from typing import Union
 
-import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 from colorama import Fore, Back, Style
 from sklearn.metrics import accuracy_score
-from sklearn.model_selection import train_test_split
 from tensorflow.keras.metrics import binary_accuracy
 
 import config
@@ -24,7 +21,7 @@ from core.data_processor import DataLoader
 from core.model import LSTMModel, TreeEnsemble, WeightedEnsemble, MixedEnsemble
 from core.utils import plot_train_val, get_most_recent_file, CSVWriter, \
     check_data_conformity, annualize_metric, Timer, calc_sharpe, \
-    calc_excess_returns, calc_sortino, add_to_json, get_model_parent_type, ProgressBar
+    calc_excess_returns, calc_sortino, add_to_json, get_model_parent_type, ProgressBar, check_directory_for_file
 
 
 def main(index_id='150095', index_name='', full_data=None, constituency_matrix: pd.DataFrame = None,
@@ -284,15 +281,25 @@ def main(index_id='150095', index_name='', full_data=None, constituency_matrix: 
 
         print('Done testing individual models.')
 
-        predictions, y_test, test_data_index = model.predict(x_test, weighted=True, test_data_index=test_data_index,
+        predictions, y_test, test_data_index = model.predict(x_test, all_predictions=all_predictions, weighted=True,
+                                                             test_data_index=test_data_index,
                                                              y_test=y_test)
 
     # JOB: Test model
-    test_model(predictions=predictions, configs=configs, folder_path=folder_path, test_data_index=test_data_index,
-               y_test=y_test, study_period_data=study_period_data.copy(), parent_model_type=parent_model_type,
-               model_type=model.model_type, history=history,
-               index_id=index_id, index_name=index_name, study_period_length=len(full_date_range), model=model,
-               period_range=period_range, start_date=start_date, end_date=end_date, plotting=plotting)
+    if len(predictions) > 1:
+        for weighting, sub_predictions in predictions:
+            test_model(predictions=sub_predictions, configs=configs, folder_path=folder_path,
+                       test_data_index=test_data_index,
+                       y_test=y_test, study_period_data=study_period_data.copy(), parent_model_type=parent_model_type,
+                       model_type=f'{model.model_type}_{weighting}', history=history,
+                       index_id=index_id, index_name=index_name, study_period_length=len(full_date_range), model=model,
+                       period_range=period_range, start_date=start_date, end_date=end_date, plotting=plotting)
+    else:
+        test_model(predictions=predictions, configs=configs, folder_path=folder_path, test_data_index=test_data_index,
+                   y_test=y_test, study_period_data=study_period_data.copy(), parent_model_type=parent_model_type,
+                   model_type=model.model_type, history=history,
+                   index_id=index_id, index_name=index_name, study_period_length=len(full_date_range), model=model,
+                   period_range=period_range, start_date=start_date, end_date=end_date, plotting=plotting)
 
     del study_period_data
 
@@ -318,6 +325,7 @@ def preprocess_data(study_period_data: pd.DataFrame, unique_indices: pd.MultiInd
     :return: Tuple of (x_train, y_train, x_test, y_test, test_data_index, data_cols, [Optional](validation data tuple))
     """
 
+    cols = cols.copy()
     print(f'{Style.BRIGHT}{Fore.YELLOW}Pre-processing data ...{Style.RESET_ALL}')
     timer = Timer().start()
 
@@ -476,8 +484,14 @@ def test_model(predictions: np.array, configs: dict, folder_path: str, test_data
         y_test = y_test[kwargs['model_index']]
         test_data_index = test_data_index[kwargs['model_index']]
         study_period_data = study_period_data.copy()
+    else:
+        study_period_data = study_period_data.copy()
 
-    print(f'\nTesting {model} model on unseen data ...')
+    if model_type:
+        print(f'\n{Style.BRIGHT}{Fore.BLUE}Testing {model_type} model on unseen data ...{Style.RESET_ALL}')
+    else:
+        print(f'\nTesting {model} model on unseen data ...')
+
     timer = Timer().start()
     # JOB: Create data frame with true and predicted values
     test_set_comparison = pd.DataFrame({'y_test': y_test.astype('int8').flatten(), 'prediction': predictions},
@@ -513,7 +527,7 @@ def test_model(predictions: np.array, configs: dict, folder_path: str, test_data
     print(f'Average size of cross sections: {int(cross_section_size)}')
 
     # Define top k values
-    top_k_list = [5, 10]
+    top_k_list = [10]
 
     if cross_section_size > 30:
         top_k_list.extend([50, 100, 150, 200, 250])
@@ -534,8 +548,18 @@ def test_model(predictions: np.array, configs: dict, folder_path: str, test_data
         # print(full_portfolio.head())
         # print(full_portfolio.tail())
 
-        annualized_sharpe = calc_sharpe(full_portfolio.loc[:, ['daily_return']].groupby(level=['datadate']).mean(), annualize=True)
-        annualized_sortino = calc_sortino(full_portfolio.loc[:, ['daily_return']].groupby(level=['datadate']).mean(), annualize=True)
+        if not get_val_score_only:
+            return_series = full_portfolio.loc[:, 'daily_return'].groupby(level=['datadate']).mean()
+            cumulative_return = (return_series + 1).cumprod().rename('Cumulative Return')
+            cumulative_return.index.name = 'Time'
+            cumulative_return.plot(title=model_type)
+            plt.legend(loc='best')
+            plt.show()
+
+        annualized_sharpe = calc_sharpe(full_portfolio.loc[:, ['daily_return']].groupby(level=['datadate']).mean(),
+                                        annualize=True)
+        annualized_sortino = calc_sortino(full_portfolio.loc[:, ['daily_return']].groupby(level=['datadate']).mean(),
+                                          annualize=True)
 
         accuracy = None
         mean_daily_return = None
@@ -667,6 +691,7 @@ def test_model(predictions: np.array, configs: dict, folder_path: str, test_data
         'Top-k Annualized Sortino': top_k_metrics['Annualized Sortino'].to_dict(),
         'Model Configs': model.get_params(),
         'Total Epochs': total_epochs,
+        'Study Period ID': config.study_period_id,
         'Period Range': period_range,
         'Study Period Start Date': start_date.isoformat(),
         'Study Period End Date': end_date.isoformat()
@@ -701,6 +726,13 @@ def test_model(predictions: np.array, configs: dict, folder_path: str, test_data
     logger = CSVWriter(output_path=os.path.join(ROOT_DIR, 'data', 'training_log.csv'),
                        field_names=list(data_record.keys()))
     logger.add_line(data_record)
+
+    external_file_path = r'E:\OneDrive - student.kit.edu\[01] Studium\[06] Seminare\[04] Electronic Markets & User Behavior\[04] Live Data'
+    if os.path.exists(external_file_path):
+        logger = CSVWriter(output_path=os.path.join(external_file_path, 'training_log.csv'),
+                           field_names=list(data_record.keys()))
+        logger.add_line(data_record)
+        add_to_json(data_record_json, os.path.join(external_file_path, 'training_log.json'))
 
     add_to_json(data_record_json, 'data/training_log.json')
 

core/model.py
@@ -1,6 +1,6 @@
 import inspect
-from typing import List
 
+from scipy.stats import rankdata
 from sklearn import ensemble
 
 from config import ROOT_DIR
@@ -11,7 +11,7 @@ GPU_ENABLED = True
 import os
 import numpy as np
 import datetime as dt
-from numpy import newaxis
+from numpy import newaxis, ndarray
 import pandas as pd
 import pprint
 from tensorflow.keras import optimizers
@@ -81,6 +81,9 @@ class LSTMModel:
             # Get layer type string
             layer_type = layer['type'].lower()
             # Add layer with configuration
+            if layer.get('params').get('input_shape'):
+                layer['params']['input_shape'] = (
+                    layer['params']['input_shape'][0], len(self.configs.get('data').get('columns')))
             self.model.add(layer_type_dict[layer_type].from_config(layer['params']))
 
             # if layer['type'] == 'lstm':
@@ -335,7 +338,8 @@ class TreeEnsemble:
         self.parameters = None
 
     def __repr__(self):
-        return f'{Style.BRIGHT}{Fore.YELLOW}{self.model}{Style.RESET_ALL}'
+        # return f'{Style.BRIGHT}{Fore.YELLOW}{self.model}{Style.RESET_ALL}'
+        return f'{Style.BRIGHT}{Fore.YELLOW}{self.model_type}{Style.RESET_ALL}'
 
     def fit(self, x_train: np.array, y_train: np.array, **fit_args):
         """
@@ -349,7 +353,7 @@ class TreeEnsemble:
         if fit_args.get('x_val'):
             val_score = execute.test_model(
                 predictions=self.model.predict_proba(fit_args.get('x_val')[fit_args['model_index']])[:, 1],
-                get_val_score_only=True, **fit_args)
+                get_val_score_only=True, model=self, **fit_args)
             return val_score
         ret_val = None
         if hasattr(self.model, 'oob_score_'):
@@ -407,6 +411,10 @@ class WeightedEnsemble:
 
     def __init__(self, index_name: str = '', classifier_type_list: List[str] = None, configs: dict = None,
                  verbose: int = 0):
+        """
+
+        :rtype: object
+        """
         self.classifier_types = classifier_type_list
         self.classifiers = [TreeEnsemble(index_name=index_name, model_type=m_type).build_model(configs, verbose=verbose)
                             for m_type in
@@ -583,11 +591,15 @@ class MixedEnsemble:
 
         return np.array(predictions_set)
 
-    def predict(self, x_test: np.array, test_data_index: np.array, y_test: np.array, weighted=False,
+    def predict(self, x_test: ndarray, all_predictions: ndarray = None, test_data_index: ndarray = None,
+                y_test: ndarray = None, weighted=False, performance_based=True, rank_based=True,
                 alpha=2) -> np.array:
         """
         Aggregate individual predictions
 
+        :param rank_based:
+        :param performance_based:
+        :param all_predictions:
         :param y_test:
         :param test_data_index: Array of test data indices
         :param alpha: Weighting parameter for weighted average
@@ -597,14 +609,17 @@ class MixedEnsemble:
         """
         print('Making aggregated predictions ...')
 
-        predictions = self.predict_all(x_test)
-        predictions_index_merged = pd.DataFrame()
+        if all_predictions is not None:
+            predictions = all_predictions
+        else:
+            predictions = self.predict_all(x_test)
+        predictions_index_merged = None
         test_sets = []
         y_test_sets = []
 
         for i, preds in enumerate(predictions):
             # JOB: Create data frame with true and predicted values
-            predictions_indexed = pd.DataFrame({'predictions': preds},
+            predictions_indexed = pd.DataFrame({f'predictions_{i}': preds},
                                                index=pd.MultiIndex.from_tuples(test_data_index[i],
                                                                                names=['datadate', 'stock_id']))
 
@@ -615,21 +630,47 @@ class MixedEnsemble:
             y_test_sets.append(y_test_indexed)
 
         for i in range(len(test_sets) - 1):
-            predictions_index_merged = test_sets[i].merge(test_sets[i + 1], how='inner', left_index=True,
-                                                          right_index=True, suffixes=(f'_{str(i)}', f'_{str(i + 1)}'))
+            if i == 0:
+                predictions_index_merged = test_sets[i]
 
-        y_test_indexed_merged = y_test_sets[0].loc[predictions_index_merged.index]
+            predictions_index_merged = predictions_index_merged.merge(test_sets[i + 1], how='inner', left_index=True,
+                                                                      right_index=True, suffixes=(False, False))
+
+        # y_test_indexed_merged = y_test_sets[0].loc[predictions_index_merged.index]
+        y_test_indexed_merged = pd.concat(y_test_sets, join='inner').drop_duplicates().loc[
+            predictions_index_merged.index]
 
         timer = Timer().start()
 
         if weighted:
-            print(
-                f'Weighting with Validation/OOB scores [{", ".join([str(np.round(score, 3)) for score in self.val_scores])}]')
-            weights = [max(score, 0) ** alpha / max(sum([max(sc, 0) ** alpha for sc in self.val_scores]), 0) for score
-                       in
-                       self.val_scores]
-            print(f'Weights: [{", ".join([str(weight) for weight in weights])}]')
-            predictions = np.average(predictions_index_merged.values, weights=weights, axis=1)
+            predictions = []
+            if performance_based:
+                print(
+                    f'Performance-based weighting with Validation/OOB scores [{", ".join([str(np.round(score, 3)) for score in self.val_scores])}]')
+                try:
+                    weights = [max(score, 0) ** alpha / max(sum([max(sc, 0) ** alpha for sc in self.val_scores]), 0) for
+                               score
+                               in
+                               self.val_scores]
+                except ZeroDivisionError as zde:
+                    print(
+                        f'{Style.BRIGHT}{Fore.LIGHTRED_EX}All validation scores are <= 0. '
+                        f'Resorting to equal weighting.{Style.RESET_ALL}')
+                    weights = [1 / len(self.val_scores) for score in self.val_scores]
+                print(f'Weights: [{", ".join([str(weight) for weight in weights])}]')
+                preds = np.average(predictions_index_merged.values, weights=weights, axis=1)
+                predictions.append(('performance', preds))
+            if rank_based:
+                print(
+                    f'Rank-based weighting with Validation/OOB scores '
+                    f'[{", ".join([str(np.round(score, 3)) for score in self.val_scores])}]')
+
+                ranks = (len(self.val_scores) + 1) - rankdata(self.val_scores, method='ordinal')
+                weights = [(1 / rank_i) / sum([(1 / rank_j) for rank_j in ranks]) for rank_i in ranks]
+
+                print(f'Weights: [{", ".join([str(round(weight, 3)) for weight in weights])}]')
+                preds = np.average(predictions_index_merged.values, weights=weights, axis=1)
+                predictions.append(('rank', preds))
         else:
             predictions = np.mean(predictions_index_merged.values, axis=1)
         timer.stop()